53ea634d45a6e4d99a7bdfbaf0f7883c46d725c4
TessYKLEE <hiway60510@gmail.com>
2022-03-12 17:24:43 +0900

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by YOONKYUNG LEE on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
