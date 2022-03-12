b51f4dd8f7bd08bac4a427b891e068bb4ff5d170
TessYKLEE <hiway60510@gmail.com>
2022-03-12 19:32:45 +0900

    Initial Commit

---
+++ loop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  loop
+//
+//  Created by YOONKYUNG LEE on 2022/03/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ loop/loopApp.swift
@@ -0,0 +1,17 @@
+//
+//  loopApp.swift
+//  loop
+//
+//  Created by YOONKYUNG LEE on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct loopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
