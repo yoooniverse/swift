ee8a7d6b73e616d738bc4fc68599a1d8dd2d7152
TessYKLEE <hiway60510@gmail.com>
2022-03-12 19:55:00 +0900

    second swift project

--- loop/ContentView.swift
+++ loop/ContentView.swift
@@ -1,3 +1,4 @@
+
 //
 //  ContentView.swift
 //  loop
@@ -7,15 +8,37 @@
 
 import SwiftUI
 
+struct Friend: Identifiable {
+    var id: Int
+    var name: String
+}
+
 struct ContentView: View {
+    let friends: [Friend] =
+        [Friend(id:0, name: "Jay"),
+        Friend(id:1, name: "Mark"),
+        Friend(id:2, name: "Thomas"),
+        Friend(id:3, name: "Myself")]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(friends) { friend in
+                NavigationLink(destination:DetailView()) {
+                Text(friend.name)
+            }
+            }.navigationBarTitle(Text("My friends"))
+        }
     }
 }
 
+struct DetailView: View {
+    var body: some View{
+        Text("Hello")
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
